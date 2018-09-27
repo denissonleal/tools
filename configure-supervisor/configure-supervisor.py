a23f9f2e3e0e841a59555f426257952b16544944
italo.andre2007 <italo.andre2007@gmail.com>
2018-09-27 14:33:54 -0300

    little adjustments

--- configure-supervisor/configure-supervisor.py
+++ configure-supervisor/configure-supervisor.py
@@ -4,6 +4,10 @@ import sys
 import os.path
 import subprocess
 
+if len(sys.argv) < 2:
+	print('ERROR: {path} {?domain}\n')
+	exit(1)
+
 numprocs = input('What is the number of processes?[1]: ')
 if(numprocs == ''):
 	numprocs = 1
@@ -12,10 +16,6 @@ path_default = sys.argv[1].split('/')
 name = input('What is the name of the program?[{}]:'.format(path_default[-1]))
 if(name == ''):
 	name = path_default[-1]
-
-if len(sys.argv) < 1:
-	print('ERROR: {name} {path} {?domain}\n')
-	exit(1)
 
 path = sys.argv[1]
 
@@ -26,7 +26,6 @@ if os.path.exists(path_supervisor):
 	exit(1)
 
 conf = """
-
 [program:{name}]
 process_name=%(program_name)s_%(process_num)02d
 command=php {path}/artisan queue:work --sleep=3 --tries=3 --timeout=300
@@ -36,14 +35,11 @@ user=www-data
 numprocs={numprocs}
 redirect_stderr=true
 stdout_logfile=/tmp/{name}.log
-
 """.format(path = path, name = name, numprocs = numprocs)
-
-print(conf)
 
 with open(path_supervisor, 'w') as file:
 	file.write(conf)
 
 subprocess.check_call(['supervisorctl', 'update'])
 
-print("Site configurado...\n\n")
+print("Supervisor configurado...\n\n")
